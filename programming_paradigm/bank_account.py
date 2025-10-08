4bb4b3f4e98724412bbd3f425fec0a45415d2675
OluchukwuOkafor <okaforoluchukwu65@gmail.com>
2025-10-08 15:42:34 +0100

    Fix BankAccount methods to match ALX checker output

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,23 +1,17 @@
 class BankAccount:
-    def __init__(self, owner, balance=0):
-        self.owner = owner
+    def __init__(self, balance=0):
         self.balance = balance
 
     def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            print(f"Deposited {amount}. Current Balance: {self.balance}")
-        else:
-            print("Deposit amount must be positive.")
+        self.balance += amount
+        print(f"Deposited: ${amount}")
 
     def withdraw(self, amount):
         if amount > self.balance:
-            print("Insufficient funds.")
-        elif amount <= 0:
-            print("Withdrawal amount must be positive.")
+            return False
         else:
             self.balance -= amount
-            print(f"Withdrew {amount}. Current Balance: {self.balance}")
+            return True
 
     def display_balance(self):
-        print(f"Account owner: {self.owner}, Current Balance: {self.balance}")
+        print(f"Current Balance: ${self.balance:.2f}")
